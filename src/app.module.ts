867720a913de6f65aefa88f1dda24b1719b8b4ee
Mansur550 <mansurislam59@gmail.com>
2026-07-02 02:05:49 +0600

    User module created

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -39,7 +40,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
   
       inject: [ConfigService]
     }),
-    AuthModule],
+    AuthModule,
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
