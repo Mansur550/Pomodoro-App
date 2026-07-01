71b2a56c8652ec89b2379c706dfc54cd5349e9af
Mansur550 <mansurislam59@gmail.com>
2026-07-02 02:18:59 +0600

    User Entity

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column()
+    password: string;
+
+    @Column()
+    firstName: string;
+
+    @Column()
+    lastName: string;
+
+
+
+}
