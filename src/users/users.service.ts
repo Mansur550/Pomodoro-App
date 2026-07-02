d78d78d8a7d898ba04e786a031eca061281cb31a
Mansur550 <mansurislam59@gmail.com>
2026-07-02 11:32:50 +0600

    Create User in user repo

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User) private readonly userRepository: Repository<User>
+    ) {}
+
+    async createUser(data: User): Promise<User>{
+        const newUser = this.userRepository.create(data);
+        return await this.userRepository.save(newUser)
+    }
+    
+}
