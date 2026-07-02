d554c18f13ca96fcc12c317be48381a4c3402833
Mansur550 <mansurislam59@gmail.com>
2026-07-02 13:21:29 +0600

    jwt token return in Auth service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
 
-    constructor(private readonly usersService: UsersService) {}
+    constructor(private readonly usersService: UsersService,
+        private readonly jwtService: JwtService
+
+    ) {}
 
     async register(createUserDto: CreateUserDto)  {
 
@@ -15,7 +19,8 @@ export class AuthService {
         const hashedPassword = await bcrypt.hash(createUserDto.password,10);
         createUserDto.password = hashedPassword;
 
-        return await this.usersService.createUser(createUserDto);
+        const user = await this.usersService.createUser(createUserDto);
+        return this.jwtService.sign({id: user.id, email: user.email});  // Return the JWT token after successful registration
     }
         
 }
