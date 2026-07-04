9da028004fb58183c82b653450606ac61aaf4369
Mansur550 <mansurislam59@gmail.com>
2026-07-05 02:44:13 +0600

    profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService: AuthService) {}
+    constructor(private authService: AuthService,
+        private userService: UsersService
+
+    ) {}
 
     @Post('register')
     async register(
@@ -16,6 +20,14 @@ export class AuthController {
     }
 
     @Post('login')
+    async login(
+        @Body() loginDto: LoginDto
+    ): Promise<{token: string}>{
+        const token= await this.authService.login(loginDto);
+        return { token };
+    }
+
+    @Post('profile')
     async login(
         @Body() loginDto: LoginDto
     ): Promise<{token: string}>{
