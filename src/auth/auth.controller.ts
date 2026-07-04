d58fdf609b82711367db2f6ee65dfb3c66c84e3e
Mansur550 <mansurislam59@gmail.com>
2026-07-05 03:09:58 +0600

    profile Route with auth gurd

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { UsersService } from 'src/users/users.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
@@ -27,11 +28,15 @@ export class AuthController {
         return { token };
     }
 
-    @Post('profile')
-    async login(
-        @Body() loginDto: LoginDto
-    ): Promise<{token: string}>{
-        const token= await this.authService.login(loginDto);
-        return { token };
+    @Get('profile')
+    @UseGuards(AuthGuard('jwt')) // Protect this route with JWT authentication
+    async profile(
+        @Req() req: any
+    ): Promise<{email: string}>{
+        const user = await this.userService.findByEmail(req.user.email);
+        if(!user){
+            throw new Error('User not found');
+        }
+        return { email : user.email };
     }
 }
